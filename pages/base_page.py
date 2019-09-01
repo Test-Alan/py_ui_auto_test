f97167b5396514f6179aaec82cec8f89cdc12b99
alan_git <280262569@qq.com>
2019-09-01 23:02:32 +0800

    添加滑动

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,4 +2,48 @@ from poium import Page
 
 
 class BasePage(Page):
-    pass
+
+    # 获取屏幕的宽高
+    def get_size(self):
+        size = self.driver.get_window_size()
+        width = size['width']
+        height = size['height']
+        return width, height
+
+    # 向左滑动
+    def swipe_left(self):
+        x1 = self.get_size()[0] * 0.9
+        y1 = self.get_size()[1] / 2
+        x2 = self.get_size()[0] / 10
+        self.driver.swipe(x1, y1, x2, y1)
+
+    # 向右滑动
+    def swipe_right(self):
+        x1 = self.get_size()[0] / 10
+        y1 = self.get_size()[1] / 2
+        x2 = self.get_size()[0] * 0.9
+        self.driver.swipe(x1, y1, x2, y1)
+
+    # 向上滑动
+    def swipe_up(self):
+        x1 = self.get_size()[0] / 2
+        y1 = self.get_size()[1] * 0.9
+        y2 = self.get_size()[1] / 10
+        self.driver.swipe(x1, y1, x1, y2)
+
+    # 向下滑动
+    def swipe_down(self):
+        x1 = self.get_size()[0] / 2
+        y1 = self.get_size()[1] / 10
+        y2 = self.get_size()[1] * 0.9
+        self.driver.swipe(x1, y1, x1, y2)
+
+    # 滑动的方向
+        if direction == 'up':
+            self.swipe_up()
+        elif direction == 'down':
+            self.swipe_down()
+        elif direction == 'right':
+            self.swipe_right()
+        else:
+            self.swipe_left()
