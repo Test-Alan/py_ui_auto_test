d97c58aa0269c6a05c4dc51afa5cf4b20c233557
aaa <asdsad@163.com>
2019-09-02 16:49:41 +0800

    优化滑动

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
-from poium import Page
+from time import sleep
 
+from poium import Page
 
 class BasePage(Page):
 
@@ -11,40 +12,50 @@ class BasePage(Page):
         return width, height
 
     # 向左滑动
-    def swipe_left(self):
-        x1 = self.get_size()[0] * 0.9
-        y1 = self.get_size()[1] / 2
-        x2 = self.get_size()[0] / 10
-        self.driver.swipe(x1, y1, x2, y1)
+    def swipe_left(self, count=1, time=0.5):
+        for i in range(count):
+            x1 = self.get_size()[0] * 0.9
+            y1 = self.get_size()[1] / 2
+            x2 = self.get_size()[0] / 10
+            self.driver.swipe(x1, y1, x2, y1)
+            sleep(time)
 
     # 向右滑动
-    def swipe_right(self):
-        x1 = self.get_size()[0] / 10
-        y1 = self.get_size()[1] / 2
-        x2 = self.get_size()[0] * 0.9
-        self.driver.swipe(x1, y1, x2, y1)
+    def swipe_right(self, count=1, time=0.5):
+        for i in range(count):
+            x1 = self.get_size()[0] / 10
+            y1 = self.get_size()[1] / 2
+            x2 = self.get_size()[0] * 0.9
+            self.driver.swipe(x1, y1, x2, y1)
+            sleep(time)
 
     # 向上滑动
-    def swipe_up(self):
-        x1 = self.get_size()[0] / 2
-        y1 = self.get_size()[1] * 0.9
-        y2 = self.get_size()[1] / 10
-        self.driver.swipe(x1, y1, x1, y2)
+    def swipe_up(self, count=1, time=0.5):
+        for i in range(count):
+            x1 = self.get_size()[0] / 2
+            y1 = self.get_size()[1] * 0.9
+            y2 = self.get_size()[1] / 10
+            self.driver.swipe(x1, y1, x1, y2)
+            sleep(time)
 
     # 向下滑动
-    def swipe_down(self):
-        x1 = self.get_size()[0] / 2
-        y1 = self.get_size()[1] / 10
-        y2 = self.get_size()[1] * 0.9
-        self.driver.swipe(x1, y1, x1, y2)
+    def swipe_down(self, count=1, time=0.5):
+        for i in range(count):
+            x1 = self.get_size()[0] / 2
+            y1 = self.get_size()[1] / 10
+            y2 = self.get_size()[1] * 0.9
+            self.driver.swipe(x1, y1, x1, y2)
+            sleep(time)
 
     # 滑动的方向
-    def swipe_on(self, direction):
+    def swipe_on(self, direction, count, time):
         if direction == 'up':
-            self.swipe_up()
+            self.swipe_up(count, time)
         elif direction == 'down':
-            self.swipe_down()
+            self.swipe_down(count, time)
         elif direction == 'right':
-            self.swipe_right()
+            self.swipe_right(count, time)
         else:
-            self.swipe_left()
+            self.swipe_left(count, time)
+
+
