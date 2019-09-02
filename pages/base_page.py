cf0b40801b98b2db81277523174ae8a67edaa30a
alan_git <280262569@qq.com>
2019-09-03 00:16:10 +0800

    优化查找元素

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,9 @@
 from time import sleep
-from poium import Page
 import allure
-from poium import PageElement, PageElements
+
+from poium import Page, PageElement, PageElements
 from allure_commons.types import AttachmentType
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from utils.log_conf import init_logger
@@ -81,27 +82,45 @@ class BasePage(Page):
         except Exception as e:
             logger.info(e)
 
+    # 截图
+    # def insert_img(driver, file_name):
+    #     now_time = time.strftime("%Y_%m_%d")  # 当前时间
+    #     scre_dir = os.path.join(data_dir, "screenshots/")
+    #     path = scre_dir + now_time
+    #     if not os.path.exists(path):
+    #         os.mkdir(path)
+    #     image_dir = os.path.join(scre_dir, now_time, file_name)
+    #     print(image_dir)
+    #     driver.get_screenshot_as_file(image_dir)
 
 
 
-# 截图
-# def insert_img(driver, file_name):
-#     now_time = time.strftime("%Y_%m_%d")  # 当前时间
-#     scre_dir = os.path.join(data_dir, "screenshots/")
-#     path = scre_dir + now_time
-#     if not os.path.exists(path):
-#         os.mkdir(path)
-#     image_dir = os.path.join(scre_dir, now_time, file_name)
-#     print(image_dir)
-#     driver.get_screenshot_as_file(image_dir)
 
 
 class FindElement(PageElement):
+    # 黑名单，用来处理一些不定时的弹框。
+    black_list = []
+    # 限制查找次数
+    count = 0
+
     def get_element(self, context):
         try:
             elem = WebDriverWait(context, self.time_out).until(EC.visibility_of_element_located(self.locator))
         except:
-            return None
+            if self.count > 2:
+                raise ("已经超过最大查找次数!")
+            self.count += 1
+            # 判断黑名单元素是否出现
+            for black in self.black_list:
+                black_elements = context.find_elements(*black)
+                if len(black_elements) >= 1:
+                    black_elements[0].click()
+                    logger.info("黑名单{}元素出现，已点击!".format(black))
+                    return self.get_element(context)
+                else:
+                    logger.info("黑名单{}元素未出现!".format(black))
+            else:
+                return None
 
         else:
             try:
@@ -111,10 +130,11 @@ class FindElement(PageElement):
                 return elem
             return elem
 
-    def find(self, context):
-        if self.get_element(context) is not None:
+    def find(self, context) -> WebElement:
+        element = self.get_element(context)
+        if element is not None:
             logger.info(format(self.__class__.__name__) + "页面" + "%s对象已访问" % ':'.join(self.locator))
-            return self.get_element(context)
+            return element
 
         else:
             # 截图
@@ -122,28 +142,15 @@ class FindElement(PageElement):
             allure.attach(context.get_screenshot_as_png(), name='-'.join(self.locator) + "对象未找到截图.png", attachment_type=AttachmentType.PNG)
             # 日志
             logger.info(format(self.__class__.__name__) + "页面" + "%s对象未找到" % ':'.join(self.locator))
-            return self.get_element(context)
+            return element
 
 
 class FindElements(PageElements):
-    def get_element(self, context):
-        try:
-            elem = WebDriverWait(context, self.time_out).until(EC.visibility_of_all_elements_located(self.locator))
-        except:
-            return None
-
-        else:
-            try:
-                style_red = 'arguments[0].style.border="2px solid red"'
-                context.execute_script(style_red, elem)
-            except BaseException:
-                return elem
-            return elem
 
     def find(self, context):
         if self.get_element(context) is not None:
             logger.info(format(self.__class__.__name__) + "页面" + "%s对象已访问" % ':'.join(self.locator))
-            return self.get_element(context)
+            return context.find_elements(*self.locator)
 
         else:
             # 截图
@@ -151,6 +158,6 @@ class FindElements(PageElements):
             allure.attach(context.get_screenshot_as_png(), name='-'.join(self.locator) + "对象未找到截图.png", attachment_type=AttachmentType.PNG)
             # 日志
             logger.info(format(self.__class__.__name__) + "页面" + "%s对象未找到" % ':'.join(self.locator))
-            return self.get_element(context)
+            return []
 
 
