0bd9eeac11c5457cd127d4ead9df6c66c820a505
aaa <asdsad@163.com>
2019-09-02 16:57:45 +0800

    优化滑动

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,9 @@
 from time import sleep
-
 from poium import Page
+from utils.log_conf import init_logger
+
+logger = init_logger("log.txt")
+
 
 class BasePage(Page):
 
@@ -13,49 +16,63 @@ class BasePage(Page):
 
     # 向左滑动
     def swipe_left(self, count=1, time=0.5):
-        for i in range(count):
-            x1 = self.get_size()[0] * 0.9
-            y1 = self.get_size()[1] / 2
-            x2 = self.get_size()[0] / 10
-            self.driver.swipe(x1, y1, x2, y1)
-            sleep(time)
+        try:
+            for i in range(count):
+                x1 = self.get_size()[0] * 0.9
+                y1 = self.get_size()[1] / 2
+                x2 = self.get_size()[0] / 10
+                self.driver.swipe(x1, y1, x2, y1)
+                sleep(time)
+        except Exception as e:
+            logger.info(e)
 
     # 向右滑动
     def swipe_right(self, count=1, time=0.5):
-        for i in range(count):
-            x1 = self.get_size()[0] / 10
-            y1 = self.get_size()[1] / 2
-            x2 = self.get_size()[0] * 0.9
-            self.driver.swipe(x1, y1, x2, y1)
-            sleep(time)
+        try:
+            for i in range(count):
+                x1 = self.get_size()[0] / 10
+                y1 = self.get_size()[1] / 2
+                x2 = self.get_size()[0] * 0.9
+                self.driver.swipe(x1, y1, x2, y1)
+                sleep(time)
+        except Exception as e:
+            logger.info(e)
 
     # 向上滑动
     def swipe_up(self, count=1, time=0.5):
-        for i in range(count):
-            x1 = self.get_size()[0] / 2
-            y1 = self.get_size()[1] * 0.9
-            y2 = self.get_size()[1] / 10
-            self.driver.swipe(x1, y1, x1, y2)
-            sleep(time)
+        try:
+            for i in range(count):
+                x1 = self.get_size()[0] / 2
+                y1 = self.get_size()[1] * 0.9
+                y2 = self.get_size()[1] / 10
+                self.driver.swipe(x1, y1, x1, y2)
+                sleep(time)
+        except Exception as e:
+            logger.info(e)
 
     # 向下滑动
     def swipe_down(self, count=1, time=0.5):
-        for i in range(count):
-            x1 = self.get_size()[0] / 2
-            y1 = self.get_size()[1] / 10
-            y2 = self.get_size()[1] * 0.9
-            self.driver.swipe(x1, y1, x1, y2)
-            sleep(time)
+        try:
+            for i in range(count):
+                x1 = self.get_size()[0] / 2
+                y1 = self.get_size()[1] / 10
+                y2 = self.get_size()[1] * 0.9
+                self.driver.swipe(x1, y1, x1, y2)
+                sleep(time)
+        except Exception as e:
+            logger.info(e)
 
     # 滑动的方向
     def swipe_on(self, direction, count, time):
-        if direction == 'up':
-            self.swipe_up(count, time)
-        elif direction == 'down':
-            self.swipe_down(count, time)
-        elif direction == 'right':
-            self.swipe_right(count, time)
-        else:
-            self.swipe_left(count, time)
-
+        try:
+            if direction == 'up':
+                self.swipe_up(count, time)
+            elif direction == 'down':
+                self.swipe_down(count, time)
+            elif direction == 'right':
+                self.swipe_right(count, time)
+            else:
+                self.swipe_left(count, time)
+        except Exception as e:
+            logger.info(e)
 
